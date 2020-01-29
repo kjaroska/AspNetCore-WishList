20e819e5f7c6d31dc58cd66575b36dfc0df5ccf6
Krzysztof D. Jaroska <kjaroska@gmail.com>
2020-01-29 10:14:14 +0100

    ...

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -10,7 +10,7 @@ namespace WishList.Models
     {
         public int Id { get; set; }
         [Required]
-        [MaxLength(80)]
+        [MaxLength(50)]
         public string Description { get; set; }
     }
 }
